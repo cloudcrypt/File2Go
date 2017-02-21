922cdd377c0ff36d4cb7c67d5afe5834cf8fcfc9
Daniel D <cloudcrypt@users.noreply.github.com>
2017-02-21 16:30:36 -0700

    Added new F2G PCL

---
+++ F2G/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace F2G
+{
+    public static class Class1
+    {
+        public static string str = "Hello World!";
+    }
+}
