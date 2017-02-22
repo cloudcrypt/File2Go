d299d1fd8930b5a6c0834014064481ba2ace385e
Daniel D <cloudcrypt@users.noreply.github.com>
2017-02-21 17:50:22 -0700

    Added working EF to F2GClient

---
+++ F2GClient/Migrations/F2GContextModelSnapshot.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using F2GClient;
+
+namespace F2GClient.Migrations
+{
+    [DbContext(typeof(F2GContext))]
+    partial class F2GContextModelSnapshot : ModelSnapshot
+    {
+        protected override void BuildModel(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .HasAnnotation("ProductVersion", "1.1.0-rtm-22752")
+                .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
+
+            modelBuilder.Entity("F2G.File", b =>
+                {
+                    b.Property<int>("ID")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<string>("name");
+
+                    b.Property<DateTime>("uploaded");
+
+                    b.Property<string>("useremail");
+
+                    b.HasKey("ID");
+
+                    b.HasIndex("useremail");
+
+                    b.ToTable("Files");
+                });
+
+            modelBuilder.Entity("F2G.User", b =>
+                {
+                    b.Property<string>("email")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<DateTime>("created");
+
+                    b.Property<string>("fname");
+
+                    b.Property<string>("lname");
+
+                    b.HasKey("email");
+
+                    b.ToTable("Users");
+                });
+
+            modelBuilder.Entity("F2G.File", b =>
+                {
+                    b.HasOne("F2G.User", "user")
+                        .WithMany("Files")
+                        .HasForeignKey("useremail");
+                });
+        }
+    }
+}
