7816fbc175c36cdc084ff140d168b08a423c6a0f
Janet Leahy <j.leahy886@gmail.com>
2017-02-22 02:53:17 -0700

    Start at home page

---
+++ F2G/ViewModels/HomeViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace F2G.ViewModels
+{
+    public class HomeViewModel
+    {
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Text)]
+        [Display(Name = "File Name")]
+        public string filename { get; set; }
+
+    }
+}
--- F2GWeb/Controllers/HomeController.cs
+++ F2GWeb/Controllers/HomeController.cs
@@ -3,14 +3,40 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using F2GWeb.Services;
+using F2G.Models;
+using F2G.ViewModels;
 
 namespace F2GWeb.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly F2GContext _db;
+        private readonly IAuthService _auth;
+
+        public HomeController(F2GContext db, IAuthService auth)
+        {
+            _db = db;
+            _auth = auth;
+        }
+
+        [HttpGet]
         public IActionResult Index()
         {
             return View();
+        }
+
+
+        [HttpPost]
+        public IActionResult Index(HomeViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                //_db.Requests.Add(new Request(model));
+                //_db.SaveChanges();
+                return Content("Requests sent");
+            }
+            return View(model);
         }
 
         public IActionResult Error()
